7bf4aef2110ae7d342085ed8cd081588e9d966ff
Garrett Long WSL Ubuntu <garrettlong097@gmail.com>
2023-01-13 14:07:05 -0500

    I think it's working?

--- server.js
+++ server.js
@@ -17,24 +17,28 @@
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
-
-
-
-
+// Using the Node.js docs here "Reading Files with Node.js"
+const fs = require('fs');
+var data = fs.readFileSync('./public/index.html','utf8',(e, _) => {
+	if (e) { console.err(e); return; }
+});
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-
-
-
-
-
+const http = require('http')
+const server = http.createServer((request, result) => {
+	result.writeHead(200, {'Content-Type': 'text/html'})
+	result.end(data);
+})
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
+const argv = require('minimist')(process.argv.slice(2))
+var port = argv.port || 3000
+server.listen(port)
+console.log('Server listening on port ' + port)
 
 
 
